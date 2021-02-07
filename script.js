011bed623e5ad22d5cbdda346dd5374117afc367
webthemelab <75390272+webthemelab@users.noreply.github.com>
2021-02-08 02:32:56 +0600

    work is not complete

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ const searchMeal = ()=>{
   const searchText =  document.getElementById('search-field').value;
   document.getElementById('search-field').value;
   const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`
+
   fetch(url)
   .then(res => res.json())
-  .then(data =>displayMeals(data.meals))
+  .then(data =>displayMeals(data.meals));
 
 }
 
@@ -12,8 +13,18 @@ const displayMeals = meals =>{
   const mealsContainer = document.getElementById('meals-container');
 
   meals.forEach(meal => {
-    const li = document.createElement('li');
-    li.innerText = meal.strMeal;
-    mealsContainer.appendChild(li);
+    console.log(meal)
+    const mealsDiv = document.createElement('div');
+    mealsDiv.innerHTML = ` 
+      <div class="detail">
+      <img src="${meal.strMealThumb}">
+      <h3>${meal.strMeal}</h3>
+      </div>
+
+    `;
+    mealsContainer.appendChild(mealsDiv);
   });
+}
+const singleMeal = (strMealThumb,strInstructions) =>{
+  console.log(strMealThumb,strInstructions);
 }
