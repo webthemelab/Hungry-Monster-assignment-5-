951c63e6bd91296d77c2f2afd13ec61941c12657
webthemelab <75390272+webthemelab@users.noreply.github.com>
2021-02-08 23:34:54 +0600

    is ok

--- script.js
+++ script.js
@@ -13,18 +13,26 @@ const displayMeals = meals =>{
   const mealsContainer = document.getElementById('meals-container');
 
   meals.forEach(meal => {
-    console.log(meal)
+    console.log(meal);
     const mealsDiv = document.createElement('div');
     mealsDiv.innerHTML = ` 
       <div class="detail">
       <img src="${meal.strMealThumb}">
       <h3>${meal.strMeal}</h3>
+      <button onclick="singleMeal('${meal.strMealThumb}')">Details</button>
       </div>
 
     `;
     mealsContainer.appendChild(mealsDiv);
   });
 }
-const singleMeal = (strMealThumb,strInstructions) =>{
-  console.log(strMealThumb,strInstructions);
+
+const singleMeal = (id) =>{
+  const url =`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
+  // console.log(url);
+  fetch(url)
+  .then(response => response.json())
+  .then(data => (data.meals));
 }
+
+ 
